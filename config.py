01dc97b5e335837cefaab82c82fa4e4d99f080ea
chuck <charlesrymal@gmail.com>
2015-04-01 11:59:26 -0700

    changing pbs

--- config.py
+++ config.py
@@ -1,39 +1,6 @@
-"""
-CMAKE_MINIMUM_REQUIRED(VERSION 2.8.8)
+import pbs.classes.Static
 
-INCLUDE($ENV{HOME}/.config.cmake)
-
-FIND_PACKAGE(CMakeHelper)
-
-PROJECT(galaxy_console)
-INCLUDE(../../../../config.cmake)
-INCLUDE(cmh_build_config)
-
-SET(SHARED True)
-
-FIND_PACKAGE(galaxy 0 COMPONENTS std REQUIRED)
-
-INCLUDE(cmh_boost)
-FIND_PACKAGE(Boost 1.55 COMPONENTS
-	python
-	REQUIRED)
-
-SET(libs
-	#galaxy_std_0
-	${galaxy_LIBRARIES}
-	${Boost_LIBRARIES}
-	python2.7)
-
-message(STATUS "boost ${Boost_LIBRARIES}")
-
-INCLUDE_DIRECTORIES("/usr/include/python2.7")
-
-INCLUDE(cmh_library)
-
-ADD_SUBDIRECTORY(test)
-"""
-
-l = Static("galaxy_console")
+l = pbs.classes.Static.Static("galaxy_console", self)
 
 l.inc_dirs.append("/usr/include/python2.7")
 
